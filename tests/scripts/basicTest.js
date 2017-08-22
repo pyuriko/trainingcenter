32f0945c8db8938e4e9cacfec3f2e0d067fb2a31
pyuriko <pyuriko@pyurikos-mbp.br.ibm.com>
2017-08-22 11:05:37 -0300

    changing script

--- tests/scripts/basicTest.js
+++ tests/scripts/basicTest.js
@@ -1,15 +1,16 @@
 module.exports = {
   'Demo test GM' : function (browser) {
     browser
-      .url('https://trainingcenter-test.mybluemix.net')
+      .url('https://trainingcenter-test.mybluemix.net');
 
 
-
-      .waitForElementVisible('#querosaber-btn', 6000, true,
-        function(){}, 'O botão ficou visível')
+      browser.expect.element('#querosaber-btn').to.be.present
+      
+      //.moveTo('#querosaber-btn', 6000, true,
+      //  function(){}, 'O botão ficou visível')
       // .setValue('.searchbox.hidden-xs.hidden-sm', 'Test')
       // .waitForElementPresent('#search-page', 60000)
-      .pause(5000)
+      browser.pause(5000)
       .end();
   }
 };
